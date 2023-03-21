f940e2370ce17aed99946b48ebc140d455ddd897
tyasenpai <tantai204.10@gmail.com>
2023-03-21 17:33:38 +0700

    half done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,4 @@
-import Welcome from "./1_component/Welcome";
-import Home from "./BT_Homelayout/Home.jsx";
-import Shop from "./BT_shoplayout/Shop"
-import MSIlayout from "./BT_msi_layout/MSIlayout";
-import Databinding from "./2_Databiding/Databinding";
-import Conditionalrendering from "./3_ConditionalRendering/Conditionalrendering";
-import List from "./4_List/list";
-import Event from "./5_Event/event";
-import State from './7_state/State'
-import Selectcar from "./selectcar/selectcar";
-import Props from "./8_Props/props";
-import Shoesshop from "./BT_ShoesShop/Shoesshop";
+
 function App() {
   return (
     <div className="App">
